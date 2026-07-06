14a7f476c53976b5fd2c15218ebb8732666897c0
a-sayuo <21yuoyuoyuo@gmail.com>
2026-07-06 22:37:06 +0900

    Laravel + Sanctum + React , SPA

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ContactController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\UserController;
 
-Route::get('/contacts', [ContactController::class, 'index']);
-Route::delete('/contacts/{id}', [ContactController::class, 'destroy']);
-Route::put('/contacts/{id}', [ContactController::class, 'update']);
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/users', [UserController::class, 'index']);
+//認証が必要なAPIルート（Sanctum）
+Route::middleware('auth:sanctum')->group(function () {
+    //ユーザー情報取得（useAuthフックで使う）
+    Route::get('/user', function (\Illuminate\Http\Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/contacts', [ContactController::class, 'index']);
+    Route::delete('/contacts/{id}', [ContactController::class, 'destroy']);
+    Route::put('/contacts/{id}', [ContactController::class, 'update']);
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/users', [UserController::class, 'index']);
+});
